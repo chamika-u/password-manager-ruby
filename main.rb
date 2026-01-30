92de683cb6fda3e3ec1de1d59762795f21d40160
Chamika Udayanga <c.udayanga2k04@gmail.com>
2026-01-31 02:30:34 +0530

    Refactor credentials loading and update .gitignore for environment variables

---
+++ main.rb
@@ -0,0 +1,13 @@
+require 'yaml'
+
+# Load credentials
+credentials = YAML.load_file('credentials.yml')
+
+# Access credentials
+EMAIL = credentials['LOGIN']['EMAIL']
+PASSWORD = credentials['LOGIN']['EMAIL']
+PASSWORD VAULT = credentials['VAULT']['PASSWORD VAULT']
+
+
+
+
